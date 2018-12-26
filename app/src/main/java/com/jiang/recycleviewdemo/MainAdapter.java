ea4287641539b730251275bb6bf738b07c282775
zhangdanjiang <zhangdanjiang@corp.netease.com>
2018-12-26 21:50:03 +0800

    [Adapter修改]

--- app/src/main/java/com/jiang/recycleviewdemo/MainAdapter.java
+++ app/src/main/java/com/jiang/recycleviewdemo/MainAdapter.java
@@ -1,12 +1,23 @@
 package com.jiang.recycleviewdemo;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 public class MainAdapter extends RecyclerView.Adapter<MainBaseHolder> {
+
+    private Context mContext;
+    private List<Integer> mList;
+
+    public MainAdapter(Context context, List<Integer> typeList) {
+        this.mContext = context;
+        this.mList = typeList;
+    }
 
     private static int MAIN_TYPE = 1;
     private static int BASE_TYPE = 2;
